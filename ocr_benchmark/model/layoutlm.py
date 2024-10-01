124ab0482483055b6be8cdb5185d27a0a5c21726
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2024-10-01 18:21:02 +0200

    added preprocessing function for layoutlm input

--- ocr_benchmark/model/layoutlm.py
+++ ocr_benchmark/model/layoutlm.py
@@ -1,2 +1,23 @@
+import torch
+
 from transformers import LayoutLMTokenizer, LayoutLMForTokenClassification
 from transformers import pipeline
+
+
+def predict(layoutlm_inputs)->torch.Tensor:
+    
+    model = LayoutLMForTokenClassification.from_pretrained("microsoft/layoutlm-base-uncased")
+
+    model.eval()
+
+    with torch.no_grad():
+        outputs = model(
+            input_ids=layoutlm_inputs["input_ids"],
+            attention_mask=layoutlm_inputs["attention_mask"],
+            bbox=layoutlm_inputs["bbox"]
+        )
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+
+    return predictions
